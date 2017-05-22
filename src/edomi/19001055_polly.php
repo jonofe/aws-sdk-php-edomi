7419d91e755f72e3049d7b2fc9aeb54dfdeea9a9
root <root@edomi-dev.feld.home>
2017-05-22 19:18:28 +0200

    updated polly php script

--- src/edomi/19001055_polly.php
+++ src/edomi/19001055_polly.php
@@ -1,21 +1,21 @@
 <?php
-//require "/opt/aws-sdk-php/vendor/autoload.php";
-require "/opt/aws-sdk-php/aws.phar";
+require "/opt/php56-edomi/src/aws-sdk-php/aws.phar";
 
 date_default_timezone_set('Europe/Berlin');
 
-logging('Starting Amazon Polly Script ...');
+logging('Starting Amazon Polly Script');
 
 // if command line parameter is missing return error
 if (count($argv) != 2) {
     echo base64_encode(serialize(json_encode(array(
-        0 => "ERROR",
-        1 => '',
-        2 => ''
+        1 => "ERROR",
+        2 => '',
+        3 => '',
+        4 => 0
     ))));
     die();
 }
-
+ 
 logging($argv[1]);
 
 // read json command line parameter
@@ -24,6 +24,7 @@ $params = json_decode(unserialize(base64_decode($argv[1])), true);
 // create AWS credentials
 $credentials = new Aws\Credentials\Credentials($params['awsAccessKeyId'], $params['awsSecretKey']);
 
+try {
 // create polly client instance
 $client = new Aws\Polly\PollyClient(array(
     'version' => 'latest',
@@ -33,15 +34,33 @@ $client = new Aws\Polly\PollyClient(array(
 ));
 
 // generate audio via polly API call
-$speech = $client->synthesizeSpeech(array(
+$response = $client->synthesizeSpeech(array(
     'OutputFormat' => $params['outputFormat'],
     'Text' => $params['text'],
     'TextType' => $params['textType'],
     'VoiceId' => $params['voiceId']
 ));
+} catch (Exception $e) {
+    logging('Error: '.$e->getMessage());
+    echo base64_encode(serialize(json_encode(array(
+        1 => "ERROR",
+        2 => '',
+        3 => '',
+        4 => 0
+    ))));
+    die();
+}
 
 // retrieve audio data from polly
-$audioData = $speech->get('AudioStream')->getContents();
+if ($response->get('Error'))
+    logging("ERROR");
+else
+    logging("SUCCESS");
+
+$audioData = $response->get('AudioStream')->getContents();
+$numChars = $response->get('RequestCharacters');
+
+logging('NumChars: ' . $numChars);
 
 // Writing audio data to file
 if ($params['filename'] == 'auto')
@@ -69,25 +88,19 @@ if ($file_ok === FALSE) {
     $result = base64_encode(serialize(json_encode(array(
         1 => 'ERROR',
         2 => '',
-        3 => ''
+        3 => '',
+        4 => 0
     ))));
 } else {
     $result = base64_encode(serialize(json_encode(array(
         1 => 'OK',
         2 => $fullFilename,
-        3 => 'http://' . $params['ip'] . '/data/tmp/' . $filename
+        3 => 'http://' . $params['ip'] . '/data/tmp/' . $filename,
+        4 => $numChars
     ))));
 }
 
 echo $result;
-
-
-function fail($message)
-{
-    file_put_contents('/usr/local/edomi/www/data/log/amazon_polly.log', $message . "\n", FILE_APPEND);
-    error_log($message);
-    die();
-}
 
 function logging($message)
 {
